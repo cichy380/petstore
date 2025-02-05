ecefa0beca21ac87eb31e144027ef62405d4bae1
cichy380 <marcin.dobroszek@gmail.com>
2025-02-05 18:22:46 +0100

    add empty PetstoreComponent

---
+++ src/app/petstore/ui/petstore.component.cy.ts
@@ -0,0 +1,7 @@
+import { PetstoreComponent } from './petstore.component'
+
+describe('PetstoreComponent', () => {
+  it('should mount', () => {
+    cy.mount(PetstoreComponent)
+  })
+})
---
+++ src/app/petstore/ui/petstore.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-petstore',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './petstore.component.html',
+  styleUrl: './petstore.component.css'
+})
+export class PetstoreComponent {
+
+}
